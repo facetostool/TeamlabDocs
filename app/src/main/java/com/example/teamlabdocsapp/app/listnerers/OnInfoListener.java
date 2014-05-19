7b3ab00944651f4c83ea94816b654bf30b4f184a
facetostool <ivan.tugin@gmail.com>
2014-05-19 16:58:51 +0400

    add OnInfoListener

---
+++ app/src/main/java/com/example/teamlabdocsapp/app/listnerers/OnInfoListener.java
@@ -0,0 +1,8 @@
+package com.example.teamlabdocsapp.app.listnerers;
+
+import com.example.teamlabdocsapp.app.api.TeamlabRespose.TeamlabResponseItem;
+
+public interface OnInfoListener {
+    public void onInfoListener(TeamlabResponseItem response);
+}
+
--- app/src/main/java/com/example/teamlabdocsapp/app/listnerers/onInfoListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.teamlabdocsapp.app.listnerers;
-
-/**
- * Created by facetostool on 19.05.2014.
- */
-public class onInfoListener {
-}
